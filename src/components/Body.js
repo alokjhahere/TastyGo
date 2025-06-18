423657990acab0a0d6c981f62cc27c535a3c3220
alok <alokjha2504@gmail.com>
2025-06-18 20:18:14 +0530

    Added Online-Offline feature

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,6 +2,7 @@ import RestaurantCard from "./RestaurantCard";
 import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnineStatus";
 
 const Body =() => {
 
@@ -10,15 +11,14 @@ const Body =() => {
     const [searchText, setSearchText] = useState("");
     const [filteredRestaurant, setFilteredRestaurant] = useState([]);
     const [isTopRated, setIsTopRated] = useState(false);
-
+    
+    
     useEffect(()=>{
         fetchData();
         console.log("useEffect is called");
         
     },[])
 
-    console.log("component is rendered");
-    
     
     const fetchData = async() =>{
         const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65420&lng=77.23730&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
@@ -45,7 +45,9 @@ const Body =() => {
         }
     }
 
+    const onlineStatus = useOnlineStatus();
 
+    if(onlineStatus===false) return <h1>Please check your Internet Connection</h1>
 
     return listOFRestaurant.length ===0? (<Shimmer/>) :(
         <div className="body">
